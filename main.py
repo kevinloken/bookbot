018a98ef850e3bac05471f1936e6f19ffcc6a8e2
Kevin Loken <kevinloken@me.com>
2024-06-30 17:57:27 -0700

    letter frequency

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    freq = {}
+    low = text.lower()
+    for ch in low:
+        if not ch.isalpha():
+            continue
+        if ch in freq:
+            freq[ch] += 1
+        else:
+            freq[ch] = 1
+    return freq
+
 def main():
     with open('books/frankenstein.txt') as file:
         text = file.read()
     print(text)
+    print(f'Number of words: {count_words(text)}')
+    print(f'Frequency of characters: {count_characters(text)}')
+
 
 if __name__ == '__main__':
     main()
